acc251bf79afa9a30c8c0e7600250792244e1cae
Roshan Singh <roshan.singh@gupshup.io>
2023-04-02 20:20:34 +0530

    show Favourites

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Header from "./Components/Header"
 import List from "./Components/List"
 import StarEmpty from "./Svg/StarEmpty"
@@ -7,36 +7,77 @@ import SearchImage from "./Svg/SearchImage"
 import {nanoid} from "nanoid"
 import './App.css';
 
+
 function App() {
-  const [updated, setUpdated] = React.useState([])
+  const [friendsList, setFriendsList] = useState([])
+  const [newFriend, setNewFriend] = useState("")
+  const [filteredFriends, setFilteredFriends] = useState([])
 
-  function handleDown(event) {
-    const friends= event.target.value;
-    if (event.key==="Enter") {
-      setUpdated(prev=> {return [...updated, {id: nanoid(), name: friends, isFavourite: false, isActive: true}]})
-    }
-}   
-console.log(updated)
+  
+function handleChange(event) {
+  const { value } = event.target;
+  setNewFriend(value)
+}
 
-function handleToggle(id) {
-  setUpdated(prevArr => prevArr.map(star=> {
+function toggleFavourites(id) {
+  setFriendsList(prevArr => prevArr.map(star=> {
     return star.id === id ?
     {...star, isFavourite: !star.isFavourite} : star
   }))
 }
 
-
-const newList=updated.map((x)=>
-            <div className="list-container"> 
-                <p>{x.name}</p>
-                <button onClick={()=>handleToggle(x.id)}>
-                     {x.isFavourite? StarFilled : StarEmpty}
-                </button>
-            </div>
-      )
-
+function handleSubmit(event) {
+    event.preventDefault()
+    const friendToBeAdded = {
+      name: newFriend,
+      isFavourite: false,
+      id: nanoid()
+    } 
+    setFriendsList([
+      ...friendsList,
+      friendToBeAdded
+    ])
+    setNewFriend("")  
+}
+function handleCheck(event) {
+  setFilteredFriends() 
+}
   
+  const filter = (event) => {
+    const { value } = event.target
   
+    if (value !== '') {
+      const results = friendsList.filter((searched) => {
+        return searched.name.toLowerCase().includes(value.toLowerCase());   
+      });
+      setFilteredFriends(results);
+    } else {
+      setFilteredFriends(friendsList); 
+    } 
+  };
+  
+  const favouritesFiltered= (event) => {
+    const { checked } = event.target
+    if (checked) {
+      const favouritesList = filteredFriends.filter((starred) => starred.isFavourite===true)
+      setFilteredFriends(favouritesList);
+    } else {
+      setFilteredFriends(friendsList)
+    }
+  }
+
+  const finalList = filteredFriends.map((mapped)=> 
+  <List 
+    name={mapped.name} 
+    toggleFavourites={()=>toggleFavourites(mapped.id)} 
+    isFavourite={mapped.isFavourite} 
+    key={mapped.id}
+  />)
+  
+ 
+  useEffect(() => {
+    setFilteredFriends(friendsList)
+  }, [friendsList])
 
     return (
         <div className="container">
@@ -44,23 +85,46 @@ const newList=updated.map((x)=>
                 <h1><u>Friends List</u></h1>
                 <form className='forms'>
                     <div>
-                        <input type="checkbox"/>
-                        <label>Show Favourites</label>
+                        <input 
+                          type="checkbox" 
+                          id="favourites"
+                          checked={filteredFriends.isFavourite}
+                          name={"isFriendly"}
+                          onChange={favouritesFiltered}
+
+                        />
+                        <label htmlFor="favourites">Show Favourites</label>
                     </div>
-                    <input type="text" placeholder='Search for a friend' />
+                    <input type="text" placeholder='Search for a friend' onChange={filter}/>
                 </form>
           </header>
-          <input className="enterName"
-                   type="text"
-                   name="friend"
-                   placeholder="Enter your Friend's name"
-                   
-                   onKeyDown={handleDown}
+          <form onSubmit={handleSubmit}>
+            <input 
+              className="enterName"
+              type="text"
+              name="friend"
+              placeholder="Enter your Friend's name"
+              onChange={handleChange}
+              value={newFriend}
             />
-            
-            <div>{newList}</div>
+          </form>
+          
+           <div>
+          {finalList}
+           </div>
+          
+      
+          
         </div>
   );
 }
 
 export default App;
+// const finalList = filteredFriends.map(({ name, isFavourite, id })=> (
+//   <div key={id} className="list-container"> 
+//     <p>{name}</p>
+//     <button onClick={() => toggleFavourites(friendsList.id)}>
+//         {isFavourite? StarFilled : StarEmpty}
+//     </button>
+//   </div>
+// ))
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,11 +1,15 @@
 
+
 export default function List(props) {
-    
+  
+   
 
     return (
-        <div>
-            <h1 className="list-style">{props.friends.name}</h1>
-            
+        <div> 
+          <h1>{props.name}</h1> 
+          <button onClick={props.toggleFavourites}>
+            {props.isFavourite? "abc" : "xyz"}
+        </button>
           </div>
     )
 }
