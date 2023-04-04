4d4ff3df693d0a51e40128c0c938c2287427c90e
Roshan Singh <roshan.singh@gupshup.io>
2023-04-04 11:43:46 +0530

    delete friends

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react"
-import Header from "./Components/Header"
 import List from "./Components/List"
-import StarEmpty from "./Svg/StarEmpty"
-import StarFilled from "./Svg/StarFilled"
-import SearchImage from "./Svg/SearchImage"
 import {nanoid} from "nanoid"
 import './App.css';
-
+import { AiOutlineSearch } from "react-icons/ai"
 
 function App() {
   const [friendsList, setFriendsList] = useState([])
   const [newFriend, setNewFriend] = useState("")
   const [filteredFriends, setFilteredFriends] = useState([])
 
-  
 function handleChange(event) {
   const { value } = event.target;
   setNewFriend(value)
+}
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const friendToBeAdded = {
+    name: newFriend,
+    isFavourite: false,
+    id: nanoid()
+  } 
+    setFriendsList([
+      ...friendsList,
+      friendToBeAdded
+    ])
+    setNewFriend("")    
 }
 
 function toggleFavourites(id) {
@@ -25,106 +34,92 @@ function toggleFavourites(id) {
     {...star, isFavourite: !star.isFavourite} : star
   }))
 }
+  
+const searchedFriend = (event) => {
+  const { value } = event.target
 
-function handleSubmit(event) {
-    event.preventDefault()
-    const friendToBeAdded = {
-      name: newFriend,
-      isFavourite: false,
-      id: nanoid()
-    } 
-    setFriendsList([
-      ...friendsList,
-      friendToBeAdded
-    ])
-    setNewFriend("")  
-}
-function handleCheck(event) {
-  setFilteredFriends() 
-}
-  
-  const filter = (event) => {
-    const { value } = event.target
-  
-    if (value !== '') {
-      const results = friendsList.filter((searched) => {
-        return searched.name.toLowerCase().includes(value.toLowerCase());   
-      });
-      setFilteredFriends(results);
-    } else {
-      setFilteredFriends(friendsList); 
-    } 
-  };
-  
-  const favouritesFiltered= (event) => {
-    const { checked } = event.target
-    if (checked) {
-      const favouritesList = filteredFriends.filter((starred) => starred.isFavourite===true)
-      setFilteredFriends(favouritesList);
-    } else {
-      setFilteredFriends(friendsList)
-    }
-  }
+  if (value !== '') {
+    const results = friendsList.filter((searched) => {
+      return searched.name.toLowerCase().includes(value.toLowerCase());   
+    });
+    setFilteredFriends(results);
+  } else {
+    setFilteredFriends(friendsList); 
+  } 
+};
 
-  const finalList = filteredFriends.map((mapped)=> 
-  <List 
-    name={mapped.name} 
-    toggleFavourites={()=>toggleFavourites(mapped.id)} 
-    isFavourite={mapped.isFavourite} 
-    key={mapped.id}
-  />)
-  
- 
-  useEffect(() => {
+const favouritesFiltered= (event) => {
+  const { checked } = event.target
+  if (checked) {
+    const favouritesList = filteredFriends.filter((starred) => starred.isFavourite === true)
+    setFilteredFriends(favouritesList);
+  } else {
     setFilteredFriends(friendsList)
-  }, [friendsList])
+  }
+}
 
-    return (
-        <div className="container">
-          <header className='header'>
-                <h1><u>Friends List</u></h1>
-                <form className='forms'>
-                    <div>
-                        <input 
-                          type="checkbox" 
-                          id="favourites"
-                          checked={filteredFriends.isFavourite}
-                          name={"isFriendly"}
-                          onChange={favouritesFiltered}
+function handleConfirmDelete(id) {
+  const newState = friendsList.filter((friend)=> friend.id !== id)
+  setFriendsList(newState)
+}
 
-                        />
-                        <label htmlFor="favourites">Show Favourites</label>
-                    </div>
-                    <input type="text" placeholder='Search for a friend' onChange={filter}/>
-                </form>
-          </header>
-          <form onSubmit={handleSubmit}>
-            <input 
-              className="enterName"
-              type="text"
-              name="friend"
-              placeholder="Enter your Friend's name"
-              onChange={handleChange}
-              value={newFriend}
-            />
-          </form>
-          
-           <div>
-          {finalList}
-           </div>
-          
-      
-          
-        </div>
+useEffect(() => {
+  setFilteredFriends(friendsList)
+}, [friendsList])
+
+  return (
+    <div className="container">
+      <header className='header'>
+            <h1>F.R.I.E.N.D.S</h1>
+            <form className='forms'>
+                <div className="showFavourite">
+                    <input 
+                      type="checkbox" 
+                      id="favourites"
+                      checked={filteredFriends.isFavourite}
+                      name={"isFriendly"}
+                      onChange={favouritesFiltered}
+
+                    />
+                    <label htmlFor="favourites">Show Favourites</label>
+                </div>
+                <label className="search-label">
+                  <input 
+                    className="search-friend"
+                    type="text" 
+                    placeholder='Search for a friend' 
+                    onChange={searchedFriend}
+                  />
+                  <AiOutlineSearch />
+                </label>                    
+            </form>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input 
+            className="add-friend-input"
+            type="text"
+            name="friend"
+            placeholder="Enter your Friend's name"
+            onChange={handleChange}
+            value={newFriend}
+          />
+        </label>
+      </form>
+      <div>
+        { filteredFriends.map( (mapped) => 
+          <List 
+            name={mapped.name} 
+            toggleFavourites={ () => toggleFavourites(mapped.id)} 
+            isFavourite={mapped.isFavourite} 
+            key={mapped.id}
+            id={mapped.id}
+            handleConfirmDelete={ () => handleConfirmDelete(mapped.id)}
+          />
+        )}
+      </div> 
+    </div>
   );
 }
 
 export default App;
-// const finalList = filteredFriends.map(({ name, isFavourite, id })=> (
-//   <div key={id} className="list-container"> 
-//     <p>{name}</p>
-//     <button onClick={() => toggleFavourites(friendsList.id)}>
-//         {isFavourite? StarFilled : StarEmpty}
-//     </button>
-//   </div>
-// ))
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,15 +1,45 @@
-
+import { useState } from "react"
+import { AiFillStar, AiOutlineStar, AiFillDelete } from "react-icons/ai";
+import { TiTick } from "react-icons/ti"
+import { RxCross2 } from "react-icons/rx"
 
 export default function List(props) {
-  
-   
+  const [deleteFriend, setDeleteFriend] = useState(false)
 
-    return (
-        <div> 
-          <h1>{props.name}</h1> 
-          <button onClick={props.toggleFavourites}>
-            {props.isFavourite? "abc" : "xyz"}
-        </button>
+  function handleDelete() {
+    setDeleteFriend(prev => !prev)
+  }
+
+  return (
+    <div className="friends"> 
+      { deleteFriend ? (
+        <div className="delete-container">
+          <p className="delete-container-p">Are you sure you want to delete?</p>
+          <div className="delete-button">
+            <button onClick={handleDelete}>
+              <RxCross2 />
+            </button>
+            <button onClick={props.handleConfirmDelete}>
+              <TiTick />
+            </button>
           </div>
-    )
+        </div>
+      ) : (
+        <div className="list-container">
+          <div className="friend-name-container">
+            <h2 className="friend-name">{props.name}</h2>
+            <p className="friend-name-p">is your friend</p> 
+          </div>
+          <div className="button-container">
+            <button onClick={props.toggleFavourites}>
+              {props.isFavourite? <AiFillStar /> : <AiOutlineStar />}
+            </button>
+            <button onClick={handleDelete}>
+              <AiFillDelete />
+            </button>
+          </div>
+        </div>
+      )}
+   </div>
+  ) 
 }
